f59ba07a4f43ac133a1079c4bd24d1cf73d18a6a
Zulfannn <115606655+Zulfannn@users.noreply.github.com>
2024-09-04 20:38:20 +0700

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./*.html', './**/*.html', './*.js', './**/*.js'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')],
-};
